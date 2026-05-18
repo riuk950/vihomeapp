941d2cfb98b50ea6ed6322fa4713dd1a39e34da7
Diego A Villate J <diegovillate950@gmail.com>
2026-05-18 12:50:16 -0500

    test 🧪: correccion en pruebas

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,11 +5,16 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:vihomeapp/app/app.dart';
 import 'package:vihomeapp/presentation/providers/providers.dart';
 import 'package:vihomeapp/domain/entities/user.dart';
+import 'package:vihomeapp/domain/entities/property.dart';
+import 'package:vihomeapp/domain/entities/property_type.dart';
 
 // Mocks
 class MockAuthProvider extends ChangeNotifier implements AuthProvider {
   @override
   bool get isAuthenticated => false;
+
+  @override
+  bool get isInitialized => true;
 
   @override
   User? get user => null;
@@ -21,10 +26,40 @@ class MockAuthProvider extends ChangeNotifier implements AuthProvider {
   String? get errorMessage => null;
 
   @override
+  void clearError() {}
+
+  @override
   dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
 
 class MockPropertyProvider extends ChangeNotifier implements PropertyProvider {
+  @override
+  List<Property> get properties => [];
+
+  @override
+  List<PropertyType> get propertyTypes => [];
+
+  @override
+  PropertyType? get selectedType => null;
+
+  @override
+  bool get isLoading => false;
+
+  @override
+  String? get errorMessage => null;
+
+  @override
+  Future<void> fetchProperties() async {}
+
+  @override
+  Future<void> fetchPropertyTypes() async {}
+
+  @override
+  void selectType(PropertyType? type) {}
+
+  @override
+  void clearError() {}
+
   @override
   dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
